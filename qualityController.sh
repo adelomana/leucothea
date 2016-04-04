29b3d01b8c5e49fbcaee1ee7997ee034815195c0
Adrian de Lomana <adrianlopezgarciadelomana@gmail.com>
2016-04-04 13:48:02 -0700

    update on quality controller

--- qualityController.sh
+++ qualityController.sh
@@ -3,48 +3,48 @@
 echo 'starting filtering and formatting... '
 
 # Sample_DS-184444
-#echo 'working with 184444...'
-#cd Sample_DS-184444 
-#pearf -1 readsFile1.trimmed.fastq -2 readsFile2.trimmed.fastq
-#echo 'converting fastq into fasta...'
-#awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile1.trimmed.fastq.filtered.fastq > readsFile1.fasta
-#awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile2.trimmed.fastq.filtered.fastq > readsFile2.fasta
-#rm *garbage*
-#cd ..
-#echo ''
+echo 'working with 184444...'
+cd Sample_DS-184444 
+pearf -1 readsFile1.trimmed.fastq -2 readsFile2.trimmed.fastq
+echo 'converting fastq into fasta...'
+awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile1.trimmed.fastq.filtered.fastq > readsFile1.fasta
+awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile2.trimmed.fastq.filtered.fastq > readsFile2.fasta
+rm *garbage*
+cd ..
+echo ''
 
 # Sample_DS-184468
-#echo 'working with 184468...'
-#cd Sample_DS-184468 
-#pearf -1 readsFile1.trimmed.fastq -2 readsFile2.trimmed.fastq
-#echo 'converting fastq into fasta...'
-#awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile1.trimmed.fastq.filtered.fastq > readsFile1.fasta
-#awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile2.trimmed.fastq.filtered.fastq > readsFile2.fasta
-#rm *garbage*
-#cd ..
-#echo ''
+echo 'working with 184468...'
+cd Sample_DS-184468
+pearf -1 readsFile1.trimmed.fastq -2 readsFile2.trimmed.fastq
+echo 'converting fastq into fasta...'
+awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile1.trimmed.fastq.filtered.fastq > readsFile1.fasta
+awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile2.trimmed.fastq.filtered.fastq > readsFile2.fasta
+rm *garbage*
+cd ..
+echo ''
 
 # Sample_DS-184469
-#echo 'working with 184469...'
-#cd Sample_DS-184469 
-#pearf -1 readsFile1.trimmed.fastq -2 readsFile2.trimmed.fastq
-#echo 'converting fastq into fasta...'
-#awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile1.trimmed.fastq.filtered.fastq > readsFile1.fasta
-#awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile2.trimmed.fastq.filtered.fastq > readsFile2.fasta
-#rm *garbage*
-#cd ..
-#echo ''
+echo 'working with 184469...'
+cd Sample_DS-184469 
+pearf -1 readsFile1.trimmed.fastq -2 readsFile2.trimmed.fastq
+echo 'converting fastq into fasta...'
+awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile1.trimmed.fastq.filtered.fastq > readsFile1.fasta
+awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile2.trimmed.fastq.filtered.fastq > readsFile2.fasta
+rm *garbage*
+cd ..
+echo ''
 
 # Sample_DS-184436
-#echo 'working with 184436...'
-#cd Sample_DS-184436
-#pearf -1 readsFile1.trimmed.fastq -2 readsFile2.trimmed.fastq
-#echo 'converting fastq into fasta...'
-#awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile1.trimmed.fastq.filtered.fastq > readsFile1.fasta
-#awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile2.trimmed.fastq.filtered.fastq > readsFile2.fasta
-#rm *garbage*
-#cd ..
-#echo ''
+echo 'working with 184436...'
+cd Sample_DS-184436
+pearf -1 readsFile1.trimmed.fastq -2 readsFile2.trimmed.fastq
+echo 'converting fastq into fasta...'
+awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile1.trimmed.fastq.filtered.fastq > readsFile1.fasta
+awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile2.trimmed.fastq.filtered.fastq > readsFile2.fasta
+rm *garbage*
+cd ..
+echo ''
 
 # Sample_DS-184460
 echo 'working with 184460...'
@@ -57,4 +57,26 @@ rm *garbage*
 cd ..
 echo ''
 
-echo '... job completed.'
+# Sample_DS-184465
+echo 'working with 184465...'
+cd Sample_DS-184465
+pearf -1 readsFile1.trimmed.fastq -2 readsFile2.trimmed.fastq
+echo 'converting fastq into fasta...'
+awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile1.trimmed.fastq.filtered.fastq > readsFile1.fasta
+awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile2.trimmed.fastq.filtered.fastq > readsFile2.fasta
+rm *garbage*
+cd ..
+echo ''
+
+# Sample_DS-184466
+echo 'working with 184466...'
+cd Sample_DS-184466
+pearf -1 readsFile1.trimmed.fastq -2 readsFile2.trimmed.fastq
+echo 'converting fastq into fasta...'
+awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile1.trimmed.fastq.filtered.fastq > readsFile1.fasta
+awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' readsFile2.trimmed.fastq.filtered.fastq > readsFile2.fasta
+rm *garbage*
+cd ..
+echo ''
+
+echo '... jobs completed.'
